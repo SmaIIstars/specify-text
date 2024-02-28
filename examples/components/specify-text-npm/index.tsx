b18958e02ca0cae20abc0b0bc1bea8e3ed8b9f91
smallstars <smallstars.he@qq.com>
2024-02-28 17:34:26 +0800

    feat: 1. example npm

--- examples/components/specify-text-npm/index.tsx
+++ examples/components/specify-text-npm/index.tsx
@@ -1,11 +1,9 @@
-// TODO: replace the default export
-// import { SpecifyText } from "specify-text";
+import SpecifyText from "specify-text";
 import widgetMap, { CustomWidgetMapProps } from "../../config/widget-map";
 import { SpecifyTextCustomType } from "../specify-text";
 
 const SpecifyTextCustom = (props: SpecifyTextCustomType) => {
-  return <div>npm</div>;
-  // return <SpecifyText<CustomWidgetMapProps> {...props} widgetMap={widgetMap} />;
+  return <SpecifyText<CustomWidgetMapProps> {...props} widgetMap={widgetMap} />;
 };
 
 export default SpecifyTextCustom;
