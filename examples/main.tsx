7aa903e0a3ea23ce59852938fbdece5a8cab8888
smallstars <smallstars.he@qq.com>
2024-03-08 11:06:31 +0800

    feat: 1. react version

--- examples/main.tsx
+++ examples/main.tsx
@@ -1,9 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
 
+// import "./assets/css/index.module.scss";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container!);
+
+// root.render(<App />);
+
+import ReactDOM from "react-dom";
+
+import App from "./App";
 import "./assets/css/index.module.scss";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
